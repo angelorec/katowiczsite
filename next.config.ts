2762253bb6a304e68cf50cf5372fd294aa28e131
angelorec <angeloreche@gmail.com>
2025-09-02 16:54:40 -0300

    Add iframe.mediadelivery.net to allowed image domains

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'iframe.mediadelivery.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
